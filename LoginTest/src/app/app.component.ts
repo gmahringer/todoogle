c5b6e0933a660f4759b57dedc29d012dfa7e12cc
rebekaangyal <aangyalrebeka@gmail.com>
2022-04-20 17:57:47 +0200

    Create login Component

--- LoginTest/src/app/app.component.ts
+++ LoginTest/src/app/app.component.ts
@@ -1,33 +1,19 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {GoogleSigninService} from "./google-signin.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
-  title = 'LoginTest';
 
-  user: gapi.auth2.GoogleUser | undefined
-
-  constructor(private signInService: GoogleSigninService, private ref : ChangeDetectorRef) {
+  constructor() {
 
   }
 
   ngOnInit(): void{
-    this.signInService.observable().subscribe(user =>{
-      this.user = user
-      this.ref.detectChanges()
-    })
-  }
-
-  signIn(){
-    this.signInService.signin()
-  }
-
-  signOut(){
-    this.signInService.signout()
   }
 
 
