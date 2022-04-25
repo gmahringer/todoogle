9ba8caa27a42dc045444eae0a06fbd21e369f2ee
rebekaangyal <aangyalrebeka@gmail.com>
2022-04-25 19:18:57 +0200

    Create task test 2

--- LoginTest/src/app/app.module.ts
+++ LoginTest/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { TodoComponent } from './todo/todo.component'
+import { TodoComponent } from './todo/todo.component';
+import { TaskComponent } from './task/task.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
--- LoginTest/src/app/todo/todo.component.ts
+++ LoginTest/src/app/todo/todo.component.ts
@@ -20,18 +20,30 @@ export class TodoComponent implements OnInit {
   /* A two-way binding performed which
      pushes text on division */
   public newTask;
+  public description;
+
+  public object1 = {
+    taskDescription: 'taskDescription',
+    startDate: Date.now(),
+    endDate: Date.now()
+  };
+
 
 
   /* When input is empty, it will
      not create a new division */
   public addToList() {
-    if (this.newTask == '') {
+    if (this.object1 == null) {
     }
     else {
-      this.items.push(this.newTask);
-      this.newTask = '';
+      this.items.push(this.object1);
+      console.log(this.items);
+      this.object1 = null;
     }
+
   }
+
+
 
   /* This function takes to input the
      task, that has to be deleted*/
