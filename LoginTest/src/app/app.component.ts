d0d7c0314cd720345d0184530fc339aa1523e652
rebekaangyal <aangyalrebeka@gmail.com>
2022-04-25 16:24:19 +0200

    Revert "Create login Component"
    This reverts commit c5b6e0933a660f4759b57dedc29d012dfa7e12cc.

--- LoginTest/src/app/app.component.ts
+++ LoginTest/src/app/app.component.ts
@@ -1,19 +1,33 @@
-import {ChangeDetectorRef, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {GoogleSigninService} from "./google-signin.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  title = 'LoginTest';
 
-  constructor() {
+  user: gapi.auth2.GoogleUser | undefined
+
+  constructor(private signInService: GoogleSigninService, private ref : ChangeDetectorRef) {
 
   }
 
   ngOnInit(): void{
+    this.signInService.observable().subscribe(user =>{
+      this.user = user
+      this.ref.detectChanges()
+    })
+  }
+
+  signIn(){
+    this.signInService.signin()
+  }
+
+  signOut(){
+    this.signInService.signout()
   }
 
 
