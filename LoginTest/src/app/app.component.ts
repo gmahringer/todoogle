e1fcd6f771bb3aed9ee7633ee7db59e128c32428
rebekaangyal <aangyalrebeka@gmail.com>
2022-04-28 16:56:16 +0200

    Google calendar event listing

--- LoginTest/src/app/app.component.ts
+++ LoginTest/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {GoogleSigninService} from "./google-signin.service";
 
 @Component({
   selector: 'app-root',
@@ -9,26 +8,18 @@ import {GoogleSigninService} from "./google-signin.service";
 export class AppComponent implements OnInit {
   title = 'LoginTest';
 
+
   user: gapi.auth2.GoogleUser | undefined
 
-  constructor(private signInService: GoogleSigninService, private ref : ChangeDetectorRef) {
+  pre: ''
+  isSignedIn = false;
 
+  constructor() {
   }
 
   ngOnInit(): void{
-    this.signInService.observable().subscribe(user =>{
-      this.user = user
-      this.ref.detectChanges()
-    })
   }
 
-  signIn(){
-    this.signInService.signin()
-  }
-
-  signOut(){
-    this.signInService.signout()
-  }
 
 
 }
